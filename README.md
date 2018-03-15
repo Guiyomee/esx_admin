80b341798f5ba174c96ec8f54db5326146e1a591
Guiyomee <guiyomee29200@gmail.com>
2018-03-15 22:23:11 +0100

    esx_admin
    Admin Menu
    
    Player
    Society
    Item
    chest admin
    ECT .....

---
+++ README.md
@@ -0,0 +1,14 @@
+# fxserver-esx_admin
+FXServer ESX Admin
+
+[INSTALLATION]
+
+1) CD in your resources/[esx] folder
+2) Clone the repository
+3) Add this in your server.cfg :
+4) Check Local FR and EN it is not complete
+
+
+```
+start esx_admin
+```
---
+++ config.lua
@@ -0,0 +1,17 @@
+Config                             			= {}
+Config.Locale                       		= 'fr'
+Config.Minutes                               = 30
+
+
+Config.SteamID = {
+	"steam:11000010c28fb52",
+	"steam:1100001192dc4ad"
+}
+
+Config.Peds ={ 
+    's_m_y_hwaycop_01',
+    's_m_y_blackops_02',
+    'csb_ramp_marine',
+    's_m_y_swat_01'
+
+}
